6cd77b9fd0cc4a1ccc5e2d4c8e1200471d1559b6
Adam <adam@dukemotorsports.net>
2021-03-10 01:08:18 -0500

    Find time between each loop

--- Pressure_Transducer/Pressure_Transducer.m
+++ Pressure_Transducer/Pressure_Transducer.m
@@ -6,8 +6,13 @@ zeroPressureVoltage = 0.5;
 maxPressureVoltage = 4.5;
 maxPressure = 500;
 
+lastTime = datetime('now');
 while true
     pressureTransudcerVoltage = readVoltage(m_arduino, pressureTransducer);
     pressure = ((pressureTransudcerVoltage - zeroPressureVoltage) / (maxPressureVoltage - zeroPressureVoltage)) * maxPressure;
-    fprintf('%.2f psi\n', pressure);
+    
+    currentTime = datetime('now');
+    dt = currentTime - lastTime;
+    fprintf('%.2f psi [%.2f ms]\n', pressure, milliseconds(dt));
+    lastTime = currentTime;
 end
