fa7835a1312fd6fc71ed7f4537ebf87835aa8ef7
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-03-12 16:18:08 -0500

    Actuator Relay

--- Pressure_Transducer/Pressure_Transducer.m
+++ Pressure_Transducer/Pressure_Transducer.m
@@ -2,6 +2,9 @@ clear; clc;
 
 m_arduino = arduino;
 pressureTransducer = 'A0';
+actuatorPin = 'D10';
+starterPin = 'D9';
+
 zeroPressureVoltage = 0.5;
 maxPressureVoltage = 4.5;
 maxPressure = 500;
@@ -10,13 +13,12 @@ actuateSignal = 0;
 lastValveTime = datetime('now');
 lastTime = datetime('now');
 while true
-    pressureTransudcerVoltage = readVoltage(m_arduino, pressureTransducer);
-    pressure = ((pressureTransudcerVoltage - zeroPressureVoltage) / (maxPressureVoltage - zeroPressureVoltage)) * maxPressure;
-    
     currentTime = datetime('now');
     dt = currentTime - lastTime;
+    
+    pressureTransudcerVoltage = readVoltage(m_arduino, pressureTransducer);
+    pressure = ((pressureTransudcerVoltage - zeroPressureVoltage) / (maxPressureVoltage - zeroPressureVoltage)) * maxPressure;
     fprintf('%.2f psi - %.2f V [%.2f ms]\n', pressure, pressureTransudcerVoltage, milliseconds(dt));
-    lastTime = currentTime;
     
     timeSinceValve = currentTime - lastValveTime;
     if (milliseconds(timeSinceValve) > 1000)
@@ -26,8 +28,10 @@ while true
            actuateSignal = 0;
        end
        
-       writeDigitalPin(m_arduino, 'D10', actuateSignal);
-       fprintf('actuate');
+       writeDigitalPin(m_arduino, actuatorPin, actuateSignal);
+       writeDigitalPin(m_arduino, starterPin, actuateSignal);
+       fprintf('actuate\n');
        lastValveTime = currentTime;
     end
+    lastTime = currentTime;
 end
