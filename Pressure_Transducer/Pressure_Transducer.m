d63431b515b6933a15ff4dc29543f7555d1d9ad5
Adam <adam@dukemotorsports.net>
2021-03-12 18:53:37 -0500

    Puff, Starter, Oxygen

--- Pressure_Transducer/Pressure_Transducer.m
+++ Pressure_Transducer/Pressure_Transducer.m
@@ -1,37 +1,54 @@
 clear; clc;
 
-m_arduino = arduino;
-pressureTransducer = 'A0';
-actuatorPin = 'D10';
-starterPin = 'D9';
+m_arduino = arduino; % Arduino Reference
+pressureTransducer = 'A0'; % Pressure Transducer (Analog 0)
+silenoidPin = 'D10'; % Silenoid for Oxygen (Digital 10)
+starterPin = 'D9'; % Relay to allow voltage to match stick starter setup (Digital 9)
 
-zeroPressureVoltage = 0.5;
-maxPressureVoltage = 4.5;
-maxPressure = 500;
+% Pressure Transducer
+maxPressure = 500; % Maximum Pressure output by pressure transducer (psi)
+zeroPressureVoltage = 0.5; % 0 psi
+maxPressureVoltage = 4.5; % maxPressure at 4.5v
 
-actuateSignal = 0;
-lastValveTime = datetime('now');
-lastTime = datetime('now');
+% Timing
+startTime = datetime('now'); % Time program starts
+lastTime = startTime; % Used for finding deltaTime in loop
+
 while true
     currentTime = datetime('now');
-    dt = currentTime - lastTime;
+    dt = milliseconds(currentTime - lastTime); % Time since last loop in milliseconds
+    dtStart = milliseconds(currentTime - startTime); % Time since start of program milliseconds
     
     pressureTransudcerVoltage = readVoltage(m_arduino, pressureTransducer);
     pressure = ((pressureTransudcerVoltage - zeroPressureVoltage) / (maxPressureVoltage - zeroPressureVoltage)) * maxPressure;
-    fprintf('%.2f psi - %.2f V [%.2f ms]\n', pressure, pressureTransudcerVoltage, milliseconds(dt));
+    fprintf('%.2f psi - %.2f V [%.2f ms]\n', pressure, pressureTransudcerVoltage, dt);
     
-    timeSinceValve = currentTime - lastValveTime;
-    if (milliseconds(timeSinceValve) > 1000)
-       if (actuateSignal == 0)
-           actuateSignal = 1;
-       else
-           actuateSignal = 0;
-       end
-       
-       writeDigitalPin(m_arduino, actuatorPin, actuateSignal);
-       writeDigitalPin(m_arduino, starterPin, actuateSignal);
-       fprintf('actuate\n');
-       lastValveTime = currentTime;
+    % Puff
+    if (dtStart < 500)
+        writeDigitalPin(m_arduino, silenoidPin, 1);
+        fprintf('oxygen\n');
+    elseif (dtStart < 2500)
+        writeDigitalPin(m_arduino, silenoidPin, 0);
+        fprintf('no oxygen\n');
     end
+    
+    % Starter
+    if (dtStart < 5000)
+        writeDigitalPin(m_arduino, starterPin, 1);
+        fprintf('starter\n');
+    else
+        writeDigitalPin(m_arduino, starterPin, 0);
+        fprintf('no starter\n');
+    end
+    
+    % Main O2 Supply
+    if (dtStart > 2500 && dtStart < 12500)
+        writeDigitalPin(m_arduino, silenoidPin, 1);
+        fprintf('oxygen\n');
+    elseif (dtStart > 2500)
+        writeDigitalPin(m_arduino, silenoidPin, 0);
+        fprintf('no oxygen\n');
+    end
+    
     lastTime = currentTime;
 end
